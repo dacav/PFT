77a4261273465a2d09d4e9bdf9ecd191fc9a21a1
Giovanni Simoni <giovanni.simoni@schibsted.com>
2016-04-01 17:21:06 +0200

    Renamed glob in Util

--- lib/PFT/Content.pm
+++ lib/PFT/Content.pm
@@ -222,7 +222,7 @@ sub _text_ls {
     my $self = shift;
 
     my @out;
-    for my $path (PFT::Util::glob @_) {
+    for my $path (PFT::Util::locale_glob @_) {
         my $hdr = eval { PFT::Header->load($path) }
             or confess "Loading header of $path: " . $@ =~ s/ at .*$//rs;
 
@@ -359,7 +359,7 @@ sub blog_back {
 
     confess 'Negative back?' if $back < 0;
 
-    my @globs = PFT::Utils::glob(
+    my @globs = PFT::Util::locale_glob(
         File::Spec->catfile($self->dir_blog, '*', '*')
     );
 
