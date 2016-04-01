692ca55a01d3830e65242b42cf63d32cbf85b106
Giovanni Simoni <giovanni.simoni@schibsted.com>
2016-04-01 08:37:21 +0200

    Conf autochecker

--- lib/PFT/Conf.pm
+++ lib/PFT/Conf.pm
@@ -17,7 +17,6 @@ PFT::Conf - Configuration parser for PFT
 =head1 SYNOPSIS
 
     PFT::Conf->new_default()        # Using default
-    PFT::Conf->new_default_env()    # Using sensible defaults
     PFT::Conf->new_load($root)      # Load from conf file in directory
     PFT::Conf->new_load_locate()    # Load from conf file, find directory
     PFT::Conf->new_load_locate($cwd)
@@ -43,106 +42,103 @@ use File::Basename qw/dirname/;
 use YAML::Tiny qw/DumpFile LoadFile/;
 
 my $CONF_NAME = 'pft.yaml';
+my($IDX_MANDATORY, $IDX_GETOPT_SUFFIX, $IDX_DEFAULT) = 0 .. 2;
 
-sub new_default {
-    bless {
-        author => 'John Doe',
-        template => 'default',
-        site_title => "My PFT website",
-        site_url => 'http://example.org/',
-        home_page => 'Welcome',
-        remote => {
-            method => 'rsync+ssh',
-            host => 'example.org',
-            port => undef,
-            user => 'user',
-            path => '/home/user/public-html/whatever',
-        },
-        system => {
-            browser => undef,
-            editor => undef,
-        },
-        input_enc => 'utf-8',
-        output_enc => 'utf-8',  # FIXME: Probably some local env?
+my $user = $ENV{USER} || 'anon';
+my %CONF_RECIPE = (
+    'site-author'     => [1, '=s', $ENV{USER} || 'Anonymous'],
+    'site-template'   => [1, '=s', 'default'],
+    'site-title'      => [1, '=s', 'My PFT website'],
+    'site-url'        => [1, '=s', 'http://example.org'],
+    'site-home'       => [1, '=s', 'Welcome'],
+    'site-encoding'   => [1, '=s', $Encode::Locale::ENCODING_LOCALE],
+    'remote-method'   => [1, '=s', 'rsync+ssh'],
+    'remote-host'     => [0, '=s', 'example.org'],
+    'remote-user'     => [0, '=s', $user],
+    'remote-port'     => [0, '=i', 22],
+    'remote-path'     => [0, '=s', $user],
+    'system-editor'   => [0, '=s', $ENV{EDITOR} || 'vim'],
+    'system-browser'  => [0, '=s', $ENV{BROWSER} || 'firefox'],
+    'system-encoding' => [0, '=s', $Encode::Locale::ENCODING_LOCALE],
+);
 
-        _root => undef,
-    }, shift;
-}
+# Transforms a flat mapping as $CONF_RECIPE into 'deep' hash table
+sub _hashify {
+    my %out;
 
-sub new_default_env {
-    my $self = shift->new_default;
-
-    $self->{author} = $ENV{USER};
-    @{$self->{system}}{'browser', 'editor'} = @ENV{'BROWSER', 'EDITOR'};
-
-    $self
-}
-
-my $check_assign = sub {
-    my $cfg = shift;
-
-    my @out;
-    for my $spec (@_) {
-        my $val = $cfg;
-        my $optional = $spec =~ /\?$/;
-        foreach (split /\./, $optional ? substr($spec, 0, -1) : $spec) {
-            last unless defined($val = $val->{$_});
+    @_ % 2 and die "Odd number of args";
+    for (my $i = 0; $i < @_; $i += 2) {
+        my @keys = split /-/, $_[$i];
+        die "Key is empty? \"$_[$i]\"" unless @keys;
+        my $dst = \%out;
+        while (@keys > 1) {
+            my $k = shift @keys;
+            $dst = exists $dst->{$k}
+                ? $dst->{$k}
+                : do { $dst->{$k} = {} };
+            ref $dst ne 'HASH' and croak "Not pointing to hash: $_[$i]";
         }
-        croak "Configuration $spec is missing"
-            unless defined($val) || $optional;
-        push @out, $val;
+        my $k = shift @keys;
+        exists $dst->{$k} and croak "Overwriting $_[$i]";
+        $dst->{$k} = $_[$i + 1];
     }
 
+    \%out;
+}
+
+sub _read_recipe {
+    my $select = shift;
+    my @out;
+    if (my $filter = shift) {
+        while (my($k, $vs) = each %CONF_RECIPE) {
+            my $v = $vs->[$select] or next;
+            push @out, $k => $vs->[$select];
+        }
+    } else {
+        while (my($k, $vs) = each %CONF_RECIPE) {
+            push @out, $k => $vs->[$select];
+        }
+    }
     @out;
-};
+}
+
+sub new_default {
+    my $self = _hashify(_read_recipe($IDX_DEFAULT));
+    $self->{_root} = undef;
+    bless $self, shift;
+}
+
+sub _check_assign {
+    my $self = shift;
+    local $" = '-';
+    my $i;
+
+    for my $mandk (grep { ++$i % 2 } _read_recipe($IDX_MANDATORY, 1)) {
+        my @keys = split /-/, $mandk;
+        my @path;
+
+        my $c = $self;
+        while (@keys > 1) {
+            push @path, (my $k = shift @keys);
+            confess "Missing @path" unless $c->{$k};
+            $c = $c->{$k};
+            confess "Path @path cannot continue with @keys"
+                unless ref $c eq 'HASH';
+        }
+        push @path, shift @keys;
+        confess "Missing @path" unless exists $c->{$path[-1]};
+    }
+}
 
 sub new_load {
     my $cls = shift;
 
     my $root = shift;
-    my $cfg = LoadFile(
+    my $self = LoadFile(
         isroot($root)
             or croak "$root is not a PFT site: $CONF_NAME is missing"
     );
-    my $self;
-
-    (
-        @{$self}{qw/
-            author
-            template
-            site_title
-            site_url
-            home_page
-            input_enc
-            output_enc
-        /},
-        @{$self->{remote}}{qw/
-            method
-            host
-            user
-            port
-            path
-        /},
-        @{$self->{system}}{qw/
-            editor
-            browser
-        /},
-    ) = $check_assign->($cfg, qw/
-        Author
-        Template
-        SiteTitle
-        SiteURL
-        HomePage
-        InputEnc
-        OutputEnc
-        Remote.Method
-        Remote.Host?
-        Remote.User?
-        Remote.Port?
-        Remote.Path?
-        System.Editor?
-        System.Browser?
-    /);
+    _check_assign($self);
     $self->{_root} = $root;
 
     bless $self, $cls;
--- t/check-conf.t
+++ t/check-conf.t
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-use v5.10;
+use v5.16;
 
 use strict;
 use warnings;
@@ -13,18 +13,72 @@ use PFT::Conf;
 use File::Spec;
 use File::Temp qw/tempdir/;
 
-my $dir = tempdir(CLEANUP => 1);
-is(PFT::Conf::locate($dir), undef);
-is(PFT::Conf::locate(), undef);
+# Checks about the underneath opts-to-hash system
 
-my $conf = PFT::Conf->new_default;
-$conf->save_to($dir);
-isnt(PFT::Conf::locate($dir), undef);
+my %hashy = (
+    site => {
+        author => 'dachiav',
+        encoding => 'utf8',
+    },
+    remote => {
+        path => 'foo/bar',
+    },
+);
+my @listy = (
+    'site-author' => 'dachiav',
+    'site-encoding' => 'utf8',
+    'remote-path' => 'foo/bar',
+);
 
-is_deeply(PFT::Conf->new_load($dir), $conf);
+is_deeply(
+    PFT::Conf::_hashify(@listy),
+    \%hashy,
+    "Hashify works"
+);
 
-mkdir File::Spec->catdir($dir, "foo") or die $!;
-mkdir my $subdir = File::Spec->catdir($dir, "foo", "bar") or die $!;
-is_deeply(PFT::Conf->new_load_locate($subdir), $conf);
+is(
+    scalar eval{PFT::Conf::_hashify(
+        'site-author' => 'self',
+        'site-author-deep' => 1,
+    )},
+    undef,
+    "Broken spec",
+);
+ok($@ =~ /\Wsite-author-deep\W/,
+    "Error message is sound"
+);
+
+is(
+    scalar eval{PFT::Conf::_hashify(
+        'site-author-deep' => 1,
+        'site-author' => 'self',
+    )},
+    undef,
+    "Broken spec",
+);
+ok($@ =~ /\Wsite-author\W/,
+    "Error message is sound"
+);
+
+do {
+    my $conf = PFT::Conf->new_default;
+    delete $conf->{site}{author};
+    is(eval{$conf->_check_assign}, undef, "Missing check");
+    ok($@ =~ /\Wsite-author\W/, "Error message is sound");
+};
+
+#my $dir = tempdir(CLEANUP => 1);
+#is(PFT::Conf::locate($dir), undef);
+#is(PFT::Conf::locate(), undef);
+#
+#my $conf = PFT::Conf->new_default;
+#$conf->save_to($dir);
+#isnt(PFT::Conf::locate($dir), undef);
+#
+#is_deeply(PFT::Conf->new_load($dir), $conf);
+#
+#mkdir File::Spec->catdir($dir, "foo") or die $!;
+#mkdir my $subdir = File::Spec->catdir($dir, "foo", "bar") or die $!;
+#is_deeply(PFT::Conf->new_load_locate($subdir), $conf);
 
 done_testing()
