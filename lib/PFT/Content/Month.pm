3c7825ce1abdbc1ef71027b6f852217cd903a0d6
Giovanni Simoni <giovanni.simoni@schibsted.com>
2016-04-08 21:58:04 +0200

    Fixed PFT::Content::{Attachment,Blog,Month,Page,Picture,Tag} for release

--- lib/PFT/Content/Month.pm
+++ lib/PFT/Content/Month.pm
@@ -1,4 +1,10 @@
-package PFT::Content::Month v0.0.1;
+package PFT::Content::Month v0.5.0;
+
+use utf8;
+use v5.16
+use strict;
+use warnings;
+
 use parent 'PFT::Content::Blog';
 
 1
--- lib/PFT/Content/Page.pm
+++ lib/PFT/Content/Page.pm
@@ -1,4 +1,10 @@
-package PFT::Content::Page v0.0.1;
+package PFT::Content::Page v0.5.0;
+
+use utf8;
+use v5.16
+use strict;
+use warnings;
+
 use parent 'PFT::Content::Entry';
 
 1
