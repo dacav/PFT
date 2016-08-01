1ef92ad17fad8c0bf0468f032459bbc26ed07e2d
Giovanni Simoni <dacav@openmailbox.org>
2016-08-01 13:44:55 +0200

    Bumped patch

--- lib/PFT.pm
+++ lib/PFT.pm
@@ -15,6 +15,6 @@
 # You should have received a copy of the GNU General Public License along
 # with PFT.  If not, see <http://www.gnu.org/licenses/>.
 #
-package PFT v1.0.1;
+package PFT v1.0.2;
 
 1;
