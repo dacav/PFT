09e0ff4132f706b7a41808c21d1e294176948e88
Giovanni Simoni <dacav@openmailbox.org>
2017-05-06 14:22:23 +0200

    Bumped minor

--- lib/PFT/Text.pm
+++ lib/PFT/Text.pm
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License along
 # with PFT.  If not, see <http://www.gnu.org/licenses/>.
 #
-package PFT::Text v1.1.2;
+package PFT::Text v1.2.0;
 
 =encoding utf8
 
